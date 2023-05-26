948343630134db55439737fcbf84ceaffa786380
Sai Karthik <110590614+sai14karthik@users.noreply.github.com>
2023-05-26 10:35:50 +0530

    Create prio_np.c

---
+++ prio_np.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+
+int main()
+{
+    int p[10], at[10], bt[10], ct[10], tat[10], wt[10], priority[10], i, j, temp = 0, n;
+    float awt = 0, atat = 0;
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    printf("Enter the arrival time, burst time, and priority for each process:\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("Process %d:\n", i + 1);
+        printf("Arrival time: ");
+        scanf("%d", &at[i]);
+        printf("Burst time: ");
+        scanf("%d", &bt[i]);
+        printf("Priority: ");
+        scanf("%d", &priority[i]);
+        p[i] = i + 1;  // Assign process ID
+    }
+
+    // Sorting processes based on priority
+    for(i = 0; i < n; i++)
+    {
+        for(j = i + 1; j < n; j++)
+        {
+            if(priority[i] > priority[j])
+            {
+                temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
+
+                temp = at[i];
+                at[i] = at[j];
+                at[j] = temp;
+
+                temp = bt[i];
+                bt[i] = bt[j];
+                bt[j] = temp;
+
+                temp = priority[i];
+                priority[i] = priority[j];
+                priority[j] = temp;
+            }
+        }
+    }
+
+    // Calculating completion time, turnaround time, and waiting time
+    ct[0] = at[0] + bt[0];
+
+    for(i = 1; i < n; i++)
+    {
+        ct[i] = ct[i - 1] + bt[i];
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        tat[i] = ct[i] - at[i];
+        wt[i] = tat[i] - bt[i];
+        atat += tat[i];
+        awt += wt[i];
+    }
+
+    atat = atat / n;
+    awt = awt / n;
+
+    // Printing process details
+    printf("\nProcess\tArrival Time\tBurst Time\tPriority\tCompletion Time\tTurnaround Time\tWaiting Time\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", p[i], at[i], bt[i], priority[i], ct[i], tat[i], wt[i]);
+    }
+
+    printf("\nAverage turnaround time: %f", atat);
+    printf("\nAverage waiting time: %f", awt);
+
+    return 0;
+}
