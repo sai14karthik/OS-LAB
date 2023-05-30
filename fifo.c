09b1baa06f69d87bcd9363a540c4cfa6f777dfb1
Sai Karthik <110590614+sai14karthik@users.noreply.github.com>
2023-05-30 18:47:18 +0530

    Update fifo.c

--- fifo.c
+++ fifo.c
@@ -1,49 +1,47 @@
-#include <stdio.h>
-int main()
-{
-int referenceString[10], pageFaults = 0, m, n, s, pages, frames;
-printf("\nEnter the number of Pages:\t");
-scanf("%d", &pages);
-printf("\nEnter reference string values:\n");
-for( m = 0; m < pages; m++)
-{
-   printf("Value No. [%d]:\t", m + 1);
-   scanf("%d", &referenceString[m]);
-}
-printf("\n What are the total number of frames:\t");
-{
-   scanf("%d", &frames);
-}
-int temp[frames];
-for(m = 0; m < frames; m++)
-{
-  temp[m] = -1;
-}
-for(m = 0; m < pages; m++)
-{
-  s = 0;
-  for(n = 0; n < frames; n++)
-   {
-      if(referenceString[m] == temp[n])
-         {
-            s++;
-            pageFaults--;
-         }
-   }     
-   pageFaults++;
-   if((pageFaults <= frames) && (s == 0))
-      {
-        temp[m] = referenceString[m];
-      }   
-   else if(s == 0)
-      {
-        temp[(pageFaults - 1) % frames] = referenceString[m];
-      }
-      printf("\n");
-      for(n = 0; n < frames; n++)
-       {     
-         printf("%d\t", temp[n]);
-       }
-} 
-printf("\nTotal Page Faults:\t%d\n", pageFaults);
-return 0;
+#include < stdio.h >  
+int main()  
+{  
+    int incomingStream[] = {4 , 1 , 2 , 4 , 5};  
+    int pageFaults = 0;  
+    int frames = 3;  
+    int m, n, s, pages;   
+    pages = sizeof(incomingStream)/sizeof(incomingStream[0]);   
+    printf(" Incoming \ t Frame 1 \ t Frame 2 \ t Frame 3 ");  
+    int temp[ frames ];  
+    for(m = 0; m < frames; m++)  
+    {  
+        temp[m] = -1;  
+    }  
+    for(m = 0; m < pages; m++)  
+    {  
+        s = 0;   
+        for(n = 0; n < frames; n++)  
+        {  
+            if(incomingStream[m] == temp[n])  
+            {  
+                s++;  
+                pageFaults--;  
+            }  
+        }  
+        pageFaults++;  
+        if((pageFaults <= frames) && (s == 0))  
+        {  
+            temp[m] = incomingStream[m];  
+        }  
+        else if(s == 0)  
+        {  
+            temp[(pageFaults - 1) % frames] = incomingStream[m];  
+        }  
+        printf("\n");  
+        printf("%d\t\t\t",incomingStream[m]);  
+        for(n = 0; n < frames; n++)  
+        {  
+            if(temp[n] != -1)  
+                printf(" %d\t\t\t", temp[n]);  
+            else  
+                printf(" - \t\t\t");  
+        }  
+    }  
+    printf("\nTotal Page Faults:\t%d\n", pageFaults);  
+    return 0;  
+}  
